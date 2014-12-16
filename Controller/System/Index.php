7b0b630174f2cd3e32f3e17370c2a0334eb1d0fb
liudng <liudng@gmail.com>
2014-12-16 16:23:36 +0800

    delete all files.

--- Controller/System/Index.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Controller_System_Index extends Smpe_Mvc_Action
-{
-	public function index() {
-		$this->layout();
-	}
-}
--- public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$workingDir = realpath(dirname(__FILE__) . '/../');
-require $workingDir . '/library/smpe/mvc/bootstrap.php';
-Smpe_Mvc_Bootstrap::run($workingDir);
